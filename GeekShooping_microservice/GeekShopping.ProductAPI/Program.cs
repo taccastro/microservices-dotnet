816970726a2504282a3e3a9900151bb69760bb26
Tiago Castro <tiago_castro@outlook.com>
2023-05-09 16:59:27 -0300

    create model produto / implementando chamadas http /

--- GeekShooping_microservice/GeekShopping.ProductAPI/Program.cs
+++ GeekShooping_microservice/GeekShopping.ProductAPI/Program.cs
@@ -21,6 +21,8 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
