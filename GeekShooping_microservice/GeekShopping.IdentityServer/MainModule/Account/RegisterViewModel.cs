c64b0c84aa7759a4e27a90092ceff265c3cc6450
Tiago Castro <tiago_castro@outlook.com>
2023-05-15 17:55:25 -0300

    Correção erro ao cadastrar User / Tratativa botão exclusão apenas para adms

--- GeekShooping_microservice/GeekShopping.IdentityServer/MainModule/Account/RegisterViewModel.cs
+++ GeekShooping_microservice/GeekShopping.IdentityServer/MainModule/Account/RegisterViewModel.cs
@@ -29,6 +29,8 @@ namespace GeekShopping.IdentityServer.MainModule.Account
         public IEnumerable<ExternalProvider> VisibleExternalProviders => ExternalProviders.Where(x => !String.IsNullOrWhiteSpace(x.DisplayName));
 
         public bool IsExternalLoginOnly => EnableLocalLogin == false && ExternalProviders?.Count() == 1;
-        public string ExternalLoginScheme => IsExternalLoginOnly ? ExternalProviders?.SingleOrDefault()?.AuthenticationScheme : null;
+
+        // Correção erro modulo 11 impletanção cad users 
+        public string? ExternalLoginScheme => IsExternalLoginOnly ? ExternalProviders?.SingleOrDefault()?.AuthenticationScheme : null;
     }
 }
