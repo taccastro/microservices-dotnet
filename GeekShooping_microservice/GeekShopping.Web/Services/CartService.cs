95430a976dcc21e5ee860620513976d4ffdd2d71
Tiago Castro <tiago_castro@outlook.com>
2023-05-16 18:30:42 -0300

    Tela Carrinho de Compras

--- GeekShooping_microservice/GeekShopping.Web/Services/CartService.cs
+++ GeekShooping_microservice/GeekShopping.Web/Services/CartService.cs
@@ -1,13 +1,7 @@
 ﻿using GeekShopping.Web.Models;
 using GeekShopping.Web.Services.IServices;
 using GeekShopping.Web.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Reflection;
-using System.Threading.Tasks;
 
 namespace GeekShopping.Web.Services
 {
